8eaf784d151debb234500ab7a62d05d8e9de6827
darkpixels201 <darkpixels201@gmail.com>
2022-11-05 11:52:57 +0500

    WorkingOnVideo

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,14 @@
  * @format
  */
 
+ const nodeLibs = require("node-libs-react-native");
+ nodeLibs.vm = require.resolve("vm-browserify");
+ 
+
 module.exports = {
+  resolver: {
+    extraNodeModules: nodeLibs
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -13,5 +20,20 @@ module.exports = {
         inlineRequires: true,
       },
     }),
+    getPolyfills: () => [
+      /**
+      * We omit RN's Object.assign polyfill
+      * If we don't, then node_modules will be using RN's polyfill rather than ours.
+      */
+      // require.resolve('react-native/Libraries/polyfills/Object.es6.js'),
+      require.resolve('react-native/Libraries/polyfills/console.js'),
+      require.resolve('react-native/Libraries/polyfills/error-guard.js'),
+      require.resolve('react-native/Libraries/polyfills/Number.es6.js'),
+      require.resolve('react-native/Libraries/polyfills/String.prototype.es6.js'),
+      require.resolve('react-native/Libraries/polyfills/Array.prototype.es6.js'),
+      require.resolve('react-native/Libraries/polyfills/Array.es6.js'),
+      require.resolve('react-native/Libraries/polyfills/Object.es7.js'),
+    ]
   },
+
 };
